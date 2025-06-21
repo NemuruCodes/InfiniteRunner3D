4241ca35aa74255325a0d880db0d61a38cbca006
Cragels <craig.harrod144@gmail.com>
2025-06-21 11:09:13 +0200

    Settings Menu
    Reworked start menu / main menu

---
+++ InfiniteRunner3D/Assets/Scripts/SettingsScript.cs
@@ -0,0 +1,79 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
+
+
+
+public class SettingScript : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    //public Dropdown resolutionDropDown;
+    public TMPro.TMP_Dropdown resolutionDropDown;
+    public TMPro.TMP_Dropdown graphicsDropDown;
+    Resolution[] resolutions;
+
+
+
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropDown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height + ", " + resolutions[i].refreshRateRatio + "Hz";
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+
+
+        resolutionDropDown.AddOptions(options);
+        resolutionDropDown.value = currentResolutionIndex;
+        resolutionDropDown.RefreshShownValue();
+
+        graphicsDropDown.value = QualitySettings.GetQualityLevel();
+        graphicsDropDown.RefreshShownValue();
+    }
+
+
+
+    public void SetReslution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+        Debug.Log(volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+        Debug.Log(qualityIndex);
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+
+}
